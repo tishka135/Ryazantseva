e082952c810d2b3ea5cdfd00b7096654ad753080
IOTNOUT12\СТУДЕНТ <tischka135>
2021-04-26 15:01:56 +0200

    Массивы 1-8

---
+++ без имени.py
@@ -0,0 +1,69 @@
+ def fil1():
+ mass= [0,0,0,0,0,0,0]
+ mass[0]=1
+ mass[-1]=1
+ print(mass)
+
+
+def fil2():
+	mass=[]
+ for i in range(10):
+	if i%2==0:
+		mass.append(0)
+	else:
+		mass.append(1)
+		
+def fil3():
+ mass = []
+ for i in range(1, 10, 2):
+	mass,append(i)
+ print(mass)
+
+
+def fil4():
+ mass = []
+ x = int(input('Введите первый элемент-')
+ d = int(input('Введите разность-'))
+ for i in range(x, x+100, d):
+	mass,append(i)
+ print(mass)
+
+
+def fil5():
+  mass = []
+ for i in range(2, 100, 2):
+	mass,append(i)
+ print(mass)
+
+
+def fil6():
+  mass = []
+ for i in range(99, 1, -3):
+	mass,append(i)
+ print(mass)
+
+def fil8():
+ n = int(input('Введите число: '))
+ for i in range()
+ print(*ass)
+ 
+def fil9():
+	
+
+change = input("введите номер задачи-")
+if change == "1":
+	fil1()
+elif change == "2":
+	fil2()
+elif change == "3":
+	fil3()
+elif change == "4":
+	fil4()
+elif change == "5":
+	fil5()
+elif change == "6":
+	fil6()
+elif change == "8":
+	fil8()
+elif change == "9":
+	fil9()
