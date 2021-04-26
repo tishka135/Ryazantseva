2b7751c5e12734488d3c84dffce819d2ffc21b7f
IOTNOUT12\СТУДЕНТ <tischka135>
2021-04-26 12:53:19 +0200

    Тест

---
+++ 81-83.py
@@ -0,0 +1,13 @@
+for i in range (100, -4, -4):
+	print(i)
+	
+b=1.2
+while b < 3.0:
+	print(round( a,2) end=" ")
+	 a +=0.2
+	
+a=25 
+for i in range ( a, a +11):
+	print( a, a+ 0.5, a- 0.2)
+	a+= 1
+ 
